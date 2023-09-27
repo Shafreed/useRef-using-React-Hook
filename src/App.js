f628f0da4e683565bc7efe03a1c9c50ddfd16522
Shafreed <shafsshafreedh321@gmail.com>
2023-09-27 09:02:32 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Comp from './Comp';
+
+
+function App() {
+  return(
+  <div className='App'>
+    <h1>useRef Hook</h1>
+    <Comp/>
+  </div>
+  )
+}
+
+export default App;
